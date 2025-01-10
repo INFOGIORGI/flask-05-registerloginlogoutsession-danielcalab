7e9b8544ba0e212b7e5fd3f0b3b30f2ad1ac8e74
danielcalab <86962732+danielcalab@users.noreply.github.com>
2025-01-10 12:29:17 +0000

    commit1

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template('home.html', titolo='Home Page')
 
-app.run()
+@app.route("/register")
+def register():
+    return render_template('register.html', titolo='Register')
+
+@app.route("/login")
+def login():
+    return render_template('login.html', titolo='Login')
+
+@app.route("/logout")
+def logout():
+    return render_template('logout.html', titolo='Logout')
+
+
+
+
+app.run(debug=True)
