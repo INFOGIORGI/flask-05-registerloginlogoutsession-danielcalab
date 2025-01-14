a15c7180bcdcfb77caa2461b1708f1271fb36fef
danielcalab <86962732+danielcalab@users.noreply.github.com>
2025-01-14 11:06:23 +0000

    commit3

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = '138.41.20.102'
+app.config['MYSQL_PORT'] = 53306
+app.config['MYSQL_USER'] = 'ospite'
+app.config['MYSQL_PASSWORD'] = 'ospite'
+app.config['MYSQL_DB'] = 'w3schools'
+
+mysql = MySQL(app)
 
 @app.route("/")
 def home():
     return render_template('home.html', titolo='Home Page')
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template('register.html', titolo='Register')
+    if request.method == 'GET':
+        return render_template('register.html', titolo='Register')
+    else:
+        nome = request.form.get("nome")
+        cognome = request.form.get("cognome")
+        username = request.form.get("username")
+        password = request.form.get('password')
+        confermaPassword = request.form.get('confermaPassword')
+        
+        
 
 @app.route("/login")
 def login():
